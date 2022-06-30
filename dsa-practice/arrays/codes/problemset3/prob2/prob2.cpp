48e4b1131b9402cc5de65f193af8d28609196d5d
tush-tr <tusharrajput604@gmail.com>
2022-06-30 05:43:19 +0530

    max contiguous subarray sum

--- dsa-practice/arrays/codes/problemset3/prob2/prob2.cpp
+++ dsa-practice/arrays/codes/problemset3/prob2/prob2.cpp
@@ -1,25 +1,57 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-
-
-int main(){
-    int arr[4] = {-1,4,7,2};
+int mxSumBruteForce(int arr[], int N)
+{
     int mxSum = INT_MIN;
-    int mxStart, mxEnd;
-    for(int i=0;i<4;i++){
-        int sum=0;
-        for(int j=i;j<4;j++){
-            sum+=arr[j];
-            if(sum>mxSum){
+    for (int i = 0; i < N; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < N; j++)
+        {
+            sum += arr[j];
+            mxSum = max(mxSum, sum);
+        }
+    }
+    return mxSum;
+}
+
+int getMxSumArray(int arr[], int N, int &mxStart, int &mxEnd)
+{
+    int mxSum = INT_MIN;
+    for (int i = 0; i < N; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < N; j++)
+        {
+            sum += arr[j];
+            if (sum > mxSum)
+            {
                 mxStart = i;
                 mxEnd = j;
                 mxSum = sum;
             }
         }
     }
-    for(int i=mxStart;i<=mxEnd;i++){
-        cout<<arr[i]<<endl;
+    return mxSum;
+}
+
+int cumulativeSum(int arr[], int N)
+{
+}
+
+int main()
+{
+    int arr[4] = {-1, 4, 7, 2};
+
+    // Brute Force method
+    int mxStart, mxEnd;
+    int mxSum = getMxSumArray(arr, 4, mxStart, mxEnd);
+    cout << "MAX SUM: " << mxSum << endl;
+    for (int i = mxStart; i <= mxEnd; i++)
+    {
+        cout << arr[i] << endl;
     }
 
+    // Optimize: Kadane's Algorithm
 }
