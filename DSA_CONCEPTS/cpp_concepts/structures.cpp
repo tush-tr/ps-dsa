6dfb0fa0a7378af217a3a41aa050a37f0027841e
tush-tr <tusharrajput604@gmail.com>
2022-08-16 22:47:33 +0530

    structure

---
+++ DSA_CONCEPTS/cpp_concepts/structures.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+// struct Rectangle{
+//     int length;
+//     int breadth;
+// } r2,r3;
+
+struct Rectangle{
+    int length;
+    int breadth;
+};
+
+// struct Rectangle r2,r3;
+
+int main(){
+    struct Rectangle r1;
+    r1.length = 134;
+    r1.breadth = 456;
+    cout<<"Area of Rectangle: "<<r1.breadth*r1.length<<endl;
+    
+
+
+}
