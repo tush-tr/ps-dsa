eb688e4ac867fd9ec60590f8cbbbc10b33cb5682
tush-tr <tusharrajput604@gmail.com>
2022-08-22 03:09:46 +0530

    tail and head recursion

--- DSA_CONCEPTS/Recursion/example1.cpp
+++ DSA_CONCEPTS/Recursion/example1.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std; 
+
+// tail recursion
 void fun(int x){
     if(x>0){
         cout<<x<<endl;
@@ -7,15 +9,25 @@ void fun(int x){
     }
 }
 
+// head recursion
 void fun2(int x){
     if(x>0){
         fun2(x-1);
         cout<<x<<endl;
     }
 }
+void fun3(int x){
+    int i=1;
+    while(i<=x){
+        cout<<i<<endl;
+        i++;
+    }
+}
+
 int main(){
     // fun(10);
     int x= 3;
-    fun(x);
+    fun2(x);
+    fun3(x);
 
 }
