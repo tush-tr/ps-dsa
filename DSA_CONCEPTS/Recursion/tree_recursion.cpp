2cfb9945ec7f03fbec488d01ac89618c7aa2dae2
tush-tr <tusharrajput604@gmail.com>
2022-08-22 04:30:39 +0530

    tree recursion

---
+++ DSA_CONCEPTS/Recursion/tree_recursion.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void fun(int x){
+    if(x>0){
+        cout<<x<<endl;
+        fun(x-1);
+        fun(x-1);
+    }
+}
+
+int main(){
+    fun(3);
+
+}
