20e4e40c07a04610af559a4ee148115240a6d01f
tush-tr <tusharrajput604@gmail.com>
2022-08-26 03:56:02 +0530

    Questions recursion

---
+++ DSA_CONCEPTS/Recursion/que1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void foo(int n, int sum)
+
+{
+
+    int k = 0, j = 0;
+
+    if (n == 0)
+        return;
+
+    k = n % 10;
+
+    j = n / 10;
+
+    sum = sum + k;
+
+    foo(j, sum);
+
+    cout << k << endl;
+}
+
+int main()
+
+{
+
+    int a = 2048, sum = 0;
+
+    foo(a, sum);
+
+    cout << sum << endl;
+}
---
+++ DSA_CONCEPTS/Recursion/que2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int &x, int c){
+    c = c - 1;
+
+    if (c == 0)
+        return 1;
+
+    x = x + 1;
+
+    return f(x, c) * x;
+}
+
+int main()
+{
+    int p = 5;
+    cout<<f(p,p)<<endl;
+}
